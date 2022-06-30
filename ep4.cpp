b7670ea87695ae0874a1f8c06a4b8d78fe13080b
heitorc62 <heitor.c@usp.br>
2022-06-30 15:39:08 -0300

    Mudança noarquivo principal para facilitar testes.

--- ep4.cpp
+++ ep4.cpp
@@ -1,19 +1,51 @@
 #include <iostream>
 #include "Reconhecedor.h"
+#include <typeinfo>
+#include <cstring>
 
 using namespace std;
 
+char* stringToCharArray(string s){
+    int n = s.length();
+    char* charArray = new char[n+1];
+    strcpy(charArray, s.c_str());
+    return charArray; 
+}
+
+
 int main(){
     char* re;
-    cin >> re;
+    string entrada = "(A*CG|A*TA|AAG*T)*";
+    //string entrada = "(A|B|C)";
+    //cin >> entrada;
+    re = stringToCharArray(entrada);
+
+    //for(int i = 0; i < a; i++) cout << re[i] << endl;
+    //cout << "Insira a expressão regular: " << endl;
+    //cin >> re;
+    /*
     int N;
+    cout << "Insira a quantidade de palavras a serem testadas: " << endl;
     cin >> N;
-    char* palavra;
+    */
     Reconhecedor reconhecedor(re);
+    string palavra;
+    char* p;
+    
+    
+    palavra = "AAAGT";
+    p = stringToCharArray(palavra);
+    if(reconhecedor.reconhece(p)) cout << "S" << endl;
+    else cout << "N" << endl;
+    
+    /*
     for(int i = 0; i < N; i++){
+        cout << "Insira a " << i+1 << " palavra: " << endl;
         cin >> palavra;
-        if(reconhecedor.reconhece(palavra)) cout << "S" << endl;
+        p = stringToCharArray(palavra);
+        if(reconhecedor.reconhece(p)) cout << "S" << endl;
         else cout << "N" << endl;
 
     }
+    */
 }
