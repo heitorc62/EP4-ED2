6c410ae23afefb25e83bd7e95f1b45cecf38ceaa
heitorc62 <heitor.c@usp.br>
2022-06-30 15:38:05 -0300

    Correção na função de reconhece e na função de construção do NFA.

--- Reconhecedor.h
+++ Reconhecedor.h
@@ -26,25 +26,45 @@ bool Reconhecedor::reconhece(char* p){
 
 Reconhecedor::Reconhecedor(char* padrao){
     re = padrao;
+    //for(int i = 0; i < strlen(re) + 1; i++) cout << re[i] << " ";
+    //cout << endl;
+    //cout << "vivo" << endl;
     nfa();
+    //G.show();
 }
 
 void Reconhecedor::nfa(){
     G = Digrafo(strlen(re) + 1); // precisa do accept state
+    //cout << "Grafo inicial: " << endl;
+    //G.show();
     stack<int> pilha;
+    stack<int> pilhaOu;
+    int ou;
     for(int i = 0; i < strlen(re); i++){
         int lp = i;
         if(re[i] == '(' || re[i] == '|'){
             pilha.push(i);
         }
         else if(re[i] == ')'){
+            while(re[pilha.top()] == '|'){
+                ou = pilha.top(); pilha.pop();
+                G.addAresta(ou, i);
+                pilhaOu.push(ou);
+            }
+            //Agora tem um lp na pilha.
+            lp = pilha.top(); pilha.pop();
+            while(!pilhaOu.empty()){
+                ou = pilhaOu.top(); pilhaOu.pop();
+                G.addAresta(lp, ou + 1);
+            }
+            /*
             int ou = pilha.top(); pilha.pop();
             if(re[ou] == '|'){
                 lp = pilha.top(); pilha.pop();
                 G.addAresta(lp, ou + 1);
                 G.addAresta(ou, i);
             }
-            else lp = ou;
+            else lp = ou;*/
         }
         if(i + 1 < strlen(re) && re[i + 1] == '*'){
             G.addAresta(lp, i + 1);
@@ -52,6 +72,13 @@ void Reconhecedor::nfa(){
         }
         if(re[i] == '(' || re[i] == '*' || re[i] == ')')
             G.addAresta(i, i + 1);
+        /*
+        cout << "Após a primeira iteração, o grafo é: " << endl;
+        G.show();
+        int opopop;
+        cout << "Insira 1 para continuar. " << endl;
+        cin >> opopop;
+        */
     }
 }
 
@@ -61,6 +88,10 @@ bool Reconhecedor::reconhece(Digrafo G, char* texto){
     for(int i = 0; i < G.V; i++) atingidos[i] = false;
     G.dfsR(0, atingidos);
     for(int i = 0; i < strlen(texto); i++){
+        /*
+        for(int opa = 0; opa < G.V; opa++) cout << atingidos[opa] << " ";
+        cout << endl;
+        */
         bool* prox = new bool[G.V];
         for(int l = 0; l < G.V; l++) prox[l] = false;
         for(int j = 0; j < G.V; j++){
@@ -70,7 +101,7 @@ bool Reconhecedor::reconhece(Digrafo G, char* texto){
         for(int j = 0; j < G.V; j++) atingidos[j] = false;
         for(int j = 0; j < G.V; j++){
             if(prox[j]){
-                for(int k = 0; k < G.V; k++) marked[k] = true;
+                for(int k = 0; k < G.V; k++) marked[k] = false;
                 G.dfsR(j, marked);
                 for(int k = 0; k < G.V; k++){
                     if(marked[k]) atingidos[k] = true;
