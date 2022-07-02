ed3ca4409e2393a7d9d40301bc77ae85eb2cb9ee
heitorc62 <heitor.c@usp.br>
2022-07-02 15:44:08 -0300

    Aparentemente resolvido o problema da barra invertida. Oq é ou não é um caractere.

--- Reconhecedor.h
+++ Reconhecedor.h
@@ -12,10 +12,12 @@ using namespace std;
 class Reconhecedor{
     public:
     char* re;
+    bool* caractere;
     Digrafo G;
     Reconhecedor(char* padrao);
     bool reconhece(char* p);
     bool reconhece(Digrafo G, char* p);
+    void caractereValido();
     void nfa();
 
 };
@@ -24,11 +26,26 @@ bool Reconhecedor::reconhece(char* p){
     return reconhece(G, p);
 }
 
+void Reconhecedor::caractereValido(){
+    caractere = new bool[strlen(re)];
+    for(int i = 0; i < strlen(re); i++) caractere[i] = true;
+    for(int i = 0; i < strlen(re); i++){
+        if(re[i] == '\\') {caractere[i] = false; i++;}
+        else{
+            if(re[i] == '(' || re[i] == ')' || re[i] == '*' || re[i] == '|') caractere[i] = false;
+        }
+    }
+}
+
 Reconhecedor::Reconhecedor(char* padrao){
     re = padrao;
     //for(int i = 0; i < strlen(re) + 1; i++) cout << re[i] << " ";
     //cout << endl;
     //cout << "vivo" << endl;
+    caractereValido();
+    for(int i = 0; i < strlen(re); i++)
+        cout << caractere[i] << " ";
+    cout << endl;
     nfa();
     //G.show();
 }
@@ -43,43 +60,36 @@ void Reconhecedor::nfa(){
     //cout << "O tamanho dessa expressão é: " << strlen(re) << endl;
     for(int i = 0; i < strlen(re); i++){
         int lp = i;
-        if(re[i] == '\\'){
-            G.addAresta(i, i + 1);
-            i++;
-        } 
-        else{
-            if(re[i] == '(' || re[i] == '|'){
-                pilha.push(i);
-            }
-            else if(re[i] == ')'){
-                while(re[pilha.top()] == '|'){
-                    ou = pilha.top(); pilha.pop();
-                    G.addAresta(ou, i);
-                    pilhaOu.push(ou);
-                }
-                //Agora tem um lp na pilha.
-                lp = pilha.top(); pilha.pop();
-                while(!pilhaOu.empty()){
-                    ou = pilhaOu.top(); pilhaOu.pop();
-                    G.addAresta(lp, ou + 1);
-                }
-                /*
-                int ou = pilha.top(); pilha.pop();
-                if(re[ou] == '|'){
-                    lp = pilha.top(); pilha.pop();
-                    G.addAresta(lp, ou + 1);
-                    G.addAresta(ou, i);
-                }
-                else lp = ou;*/
-            }
-            if(i + 1 < strlen(re) && re[i + 1] == '*'){
-                G.addAresta(lp, i + 1);
-                G.addAresta(i + 1, lp);
-            }
-            if(re[i] == '(' || re[i] == '*' || re[i] == ')')
-                G.addAresta(i, i + 1);
-
+        if((!caractere[i]) && (re[i] == '(' || re[i] == '|')){
+            pilha.push(i);
         }
+        else if((!caractere[i]) && (re[i] == ')')){
+            while(re[pilha.top()] == '|'){
+                ou = pilha.top(); pilha.pop();
+                G.addAresta(ou, i);
+                pilhaOu.push(ou);
+            }
+            //Agora tem um lp na pilha.
+            lp = pilha.top(); pilha.pop();
+            while(!pilhaOu.empty()){
+                ou = pilhaOu.top(); pilhaOu.pop();
+                G.addAresta(lp, ou + 1);
+            }
+            /*
+            int ou = pilha.top(); pilha.pop();
+            if(re[ou] == '|'){
+                lp = pilha.top(); pilha.pop();
+                G.addAresta(lp, ou + 1);
+                G.addAresta(ou, i);
+            }
+            else lp = ou;*/
+        }
+        if(i + 1 < strlen(re) && (!caractere[i + 1]) && re[i + 1] == '*'){
+            G.addAresta(lp, i + 1);
+            G.addAresta(i + 1, lp);
+        }
+        if((!caractere[i]) && (re[i] == '(' || re[i] == '*' || re[i] == ')' || re[i] == '\\'))
+            G.addAresta(i, i + 1);
         /*
         cout << "Após a primeira iteração, o grafo é: " << endl;
         G.show();
@@ -95,13 +105,7 @@ bool Reconhecedor::reconhece(Digrafo G, char* texto){
     bool* atingidos = new bool[G.V];
     for(int i = 0; i < G.V; i++) atingidos[i] = false;
     G.dfsR(0, atingidos);
-
-    for(int i = 0; i < G.V; i++){
-        if(atingidos[i]){
-            if(re[i] == '(' || re[i] == ')' || re[i] == '\\') atingidos[i] = false;
-        }
-    }
-    
+   
     for(int i = 0; i < strlen(texto); i++){
         /*
         for(int opa = 0; opa < G.V; opa++) cout << atingidos[opa] << " ";
@@ -110,7 +114,7 @@ bool Reconhecedor::reconhece(Digrafo G, char* texto){
         bool* prox = new bool[G.V];
         for(int l = 0; l < G.V; l++) prox[l] = false;
         for(int j = 0; j < G.V; j++){
-            if(atingidos[j] && re[j] == texto[i]) prox[j+1] = true;
+            if(atingidos[j] && caractere[j] && re[j] == texto[i]) prox[j+1] = true;
         }
         bool* marked = new bool[G.V];
         for(int j = 0; j < G.V; j++) atingidos[j] = false;
@@ -119,7 +123,7 @@ bool Reconhecedor::reconhece(Digrafo G, char* texto){
                 for(int k = 0; k < G.V; k++) marked[k] = false;
                 G.dfsR(j, marked);
                 for(int k = 0; k < G.V; k++){
-                    if(marked[k] && re[k] != '(' && re[k] != ')' && re[k] != '\\') atingidos[k] = true;
+                    if(marked[k]) atingidos[k] = true;
                 }
             }
         }
